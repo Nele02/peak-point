e2e9bf05857b609635c67b1653e0a7d554aeec63
Nele02 <nele.magel@t-online.de>
2025-11-14 19:06:05 +0100

    feat: serve uploaded images via /uploads rout

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import Cookie from "@hapi/cookie";
 import dotenv from "dotenv";
 import path from "path";
 import Joi from "joi";
+import Inert from "@hapi/inert";
 import { fileURLToPath } from "url";
 import Handlebars from "handlebars";
 import { webRoutes } from "./web-routes.js";
@@ -27,6 +28,7 @@ async function init() {
 
   await server.register(Vision);
   await server.register(Cookie);
+  await server.register(Inert);
   server.validator(Joi);
 
   server.views({
@@ -54,6 +56,19 @@ async function init() {
 
   server.route(webRoutes);
   server.route(apiRoutes);
+
+  server.route({
+    method: "GET",
+    path: "/uploads/{param*}",
+    options: {
+      auth: false,
+    },
+    handler: {
+      directory: {
+        path: path.join(__dirname, "..", "uploads"), // <<-- WICHTIG
+      },
+    },
+  });
 
   await server.start();
   console.log("Server running on %s", server.info.uri);
