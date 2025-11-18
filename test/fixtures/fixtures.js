417d6fe92f23572a7d0144f4ca4667faa7c7df00
Nele02 <nele.magel@t-online.de>
2025-11-18 23:03:32 +0100

    chore(seeding): implemented seeding, disabled seeding for tests with cross-env

--- test/fixtures/fixtures.js
+++ test/fixtures/fixtures.js
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
 
 const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 export const serviceUrl = "http://localhost:3000";
 
