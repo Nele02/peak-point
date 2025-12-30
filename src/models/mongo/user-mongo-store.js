0f9c2ed9b5d73133aeecb219f154184ecb846bca
Nele02 <nele.magel@t-online.de>
2025-12-30 14:10:43 +0100

    feat(auth): add bcrypt-based password hashing and validation logic

--- src/models/mongo/user-mongo-store.js
+++ src/models/mongo/user-mongo-store.js
@@ -1,5 +1,8 @@
 import Mongoose from "mongoose";
 import { User } from "./user.js";
+import bcrypt from "bcryptjs";
+
+const saltRounds = 10;
 
 export const userMongoStore = {
   async getAllUsers() {
@@ -16,7 +19,20 @@ export const userMongoStore = {
   },
 
   async addUser(user) {
+    if (!user) {
+      const newUser = new User({});
+      const userObj = await newUser.save();
+      const u = await this.getUserById(userObj._id);
+      return u;
+    }
+
     const newUser = new User(user);
+
+    if (user.password) {
+      const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+      newUser.password = hashedPassword;
+    }
+
     const userObj = await newUser.save();
     const u = await this.getUserById(userObj._id);
     return u;
